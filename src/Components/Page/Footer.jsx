7f766bd62b557f72165b9217349edc40df2f1167
minhajul08 <minhazmehadi61@gmail.com>
2024-05-06 16:13:53 +0600

    design private route

--- src/Components/Page/Footer.jsx
+++ src/Components/Page/Footer.jsx
@@ -2,13 +2,13 @@
 const Footer = () => {
     return (
         <div>
-           <footer className="footer p-10 bg-base-200 text-base-content">
+           <footer className="footer p-10 bg-base-200 text-base-content lg:justify-between lg:flex ">
   <nav>
-    <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <h6 className="footer-title">Top Destination</h6> 
+    <a className="link link-hover">Cox's Bazer</a>
+    <a className="link link-hover">Hawaii</a>
+    <a className="link link-hover">Saint Martin's</a>
+    <a className="link link-hover">New York City</a>
   </nav> 
   <nav>
     <h6 className="footer-title">Company</h6> 
